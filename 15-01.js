b899930501761fff9d569f73184ea397421ab204
dimaqqw <dimaaqqw@mail.ru>
2024-03-18 12:29:28 +0300

    first commit

---
+++ 15-01.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const app = express()
+const expbs = require('express-handlebars')
+const fs = require('fs').promises
+
+const PORT = 3000
+const path = 'data.json'
+
+app.use(express.json())
+app.use(express.static('public'))
+app.engine(
+  'handlebars',
+  expbs.engine({
+    defaultLayout: 'main',
+    helpers: {
+      declineAction: () => {
+        return "window.location.href = '/';"
+      },
+    },
+  })
+)
+app.set('view engine', 'handlebars')
+
+async function readData() {
+  const data = await fs.readFile(path, 'utf8')
+  return JSON.parse(data)
+}
+
+// Отображается форма GET:/
+app.get('/', async (req, res) => {
+  const data = await readData()
+  res.render('index', {
+    title: 'GET:/',
+    data: data || [],
+  })
+})
+// Отображается форма GET:/Add
+app.get('/Add', async (req, res) => {
+  const data = await readData()
+  res.render('add', {
+    title: 'GET:/Add',
+    data: data || [],
+  })
+})
+// Добавляет строку в json-файл, в ответе форма GET:/
+app.post('/Add', async (req, res) => {
+  console.log('post add')
+  try {
+    const id = await readData()
+    // console.log(id.length)
+    const newData = {
+      name: req.body.name,
+      number: req.body.number,
+      id: id.length,
+    }
+    const jsonData = await fs.readFile(path, 'utf8')
+    const existingData = JSON.parse(jsonData)
+    existingData.push(newData)
+    await fs.writeFile(path, JSON.stringify(existingData, null, 2))
+    res.status(200).send('Data added successfully!')
+  } catch (error) {
+    console.error('Error adding data:', error)
+    res.status(500).send('Error adding data')
+  }
+})
+// Отображается форма GET:/Update
+app.get('/Update/:name/:number/:id', async (req, res) => {
+  const data = await readData()
+  console.log(req.params.name)
+  console.log(req.params.number)
+  console.log(req.params.id)
+  const forUpdateString = {
+    name: req.params.name,
+    number: req.params.number,
+    id: req.params.id,
+  }
+  res.render('update', {
+    title: 'GET:/Update',
+    data: data || [],
+    dataForUpdateString: forUpdateString,
+  })
+})
+// Изменяет строку в json-файле, в ответе форма GET:/
+app.post('/Update', async (req, res) => {
+  try {
+    const { id, name, number } = req.body
+    const data = await readData()
+    const updatedData = data.map((item) => {
+      if (item.id === parseInt(id)) {
+        return { id: parseInt(id), name, number }
+      }
+      return item
+    })
+    await fs.writeFile(path, JSON.stringify(updatedData, null, 2))
+    res.status(200).send('Data updated successfully!')
+  } catch (error) {
+    console.error('Error updating data:', error)
+    res.status(500).send('Error updating data')
+  }
+  res.redirect('/')
+})
+
+// Удаляет строку в json-файле, в ответе форма GET:/
+app.post('/Delete', async (req, res) => {
+  try {
+    const { id } = req.body
+    const data = await readData()
+    const updatedData = data.filter((item) => item.id !== parseInt(id))
+    await fs.writeFile(path, JSON.stringify(updatedData, null, 2))
+    res.status(200).send('Data deleted successfully!')
+  } catch (error) {
+    console.error('Error deleting data:', error)
+    res.status(500).send('Error deleting data')
+  }
+  res.redirect('/')
+})
+
+app.listen(PORT, () => {
+  console.log('http://localhost:3000/')
+})
---
+++ public/scripts/main.js
@@ -0,0 +1,110 @@
+function switchGetHome() {}
+
+function switchGetAdd() {
+  window.location.href = 'Add'
+}
+
+function switchGetUpdate(name, number, id) {
+  window.location.href = 'Update/' + name + '/' + number + '/' + id
+  // var xhr = new XMLHttpRequest()
+  // xhr.open('GET', '/Update', true)
+  // xhr.setRequestHeader('Content-Type', 'application/json')
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState === XMLHttpRequest.DONE) {
+  //     if (xhr.status === 200) {
+  //       console.log('POST request successful!')
+  //       window.location.href = '/'
+  //     } else {
+  //       console.error('Error:', xhr.status)
+  //     }
+  //   }
+  // }
+}
+
+function postDataByAddButton() {
+  const dataName = document.getElementById('fio').value
+  const dataNumber = +document.getElementById('number').value
+  console.log(Number.isInteger(dataNumber))
+  if (dataName != '' && dataNumber != '') {
+    if (Number.isInteger(dataNumber)) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('POST', '/Add', true)
+      xhr.setRequestHeader('Content-Type', 'application/json')
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            console.log('POST request successful!')
+            window.location.href = '/'
+          } else {
+            console.error('Error:', xhr.status)
+          }
+        }
+      }
+      const data = {
+        name: dataName,
+        number: dataNumber,
+      }
+      xhr.send(JSON.stringify(data))
+    } else {
+      console.log('Номер некоректный')
+    }
+  } else {
+    console.log('Нет данных!')
+  }
+}
+
+if (window.location.pathname.includes('/Update')) {
+  var buttonStrings = document.querySelectorAll('.button-string')
+  buttonStrings.forEach(function (buttonString) {
+    buttonString.style.pointerEvents = 'none'
+  })
+}
+
+function postDataByUpdate() {
+  const id = document.getElementById('id').value
+  const name = document.getElementById('fio').value
+  const number = document.getElementById('number').value
+  if (id != '' && name != '' && number != '' && Number.isInteger(dataNumber)) {
+    const data = { id, name, number }
+
+    fetch('/Update', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Data updated successfully!')
+          window.location.href = '/'
+        } else {
+          console.error('Error updating data:', response.statusText)
+        }
+      })
+      .catch((error) => console.error('Error updating data:', error))
+  } else {
+    console.log('Error for update')
+  }
+}
+
+function postDataByDelete() {
+  const id = document.getElementById('id').value
+
+  fetch('/Delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Data deleted successfully!')
+        window.location.href = '/'
+      } else {
+        console.error('Error deleting data:', response.statusText)
+      }
+    })
+    .catch((error) => console.error('Error deleting data:', error))
+}
